59712c62a6c5211979497a878dfa4c195007d709
fenshitianyue <abzanda@163.com>
2019-05-25 10:44:01 +0800

    A class that tests the runtime of a code block

---
+++ time/time_interval.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+///////////////////////////////////////////////////////
+// 这是一个Linux下测试代码块运行时间的类
+///////////////////////////////////////////////////////
+#include <iostream>
+#include <sys/time.h>
+
+#define TIME_INTERVAL TimeInterval t
+
+class TimeInterval{
+public:
+  TimeInterval(){
+    gettimeofday(&start, NULL);
+  }
+  ~TimeInterval(){
+    gettimeofday(&end, NULL);
+    std::cout << 1000 * (end.tv_sec - start.tv_sec) + (start.tv_usec - end.tv_usec) / 1000 << "ms"
+              << std::endl;
+  }
+private:
+  timeval start; 
+  timeval end;
+};
+
+
